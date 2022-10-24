ade729c3f890a953ee91ee1396dbb921d2b98e57
KSKamburova18 <KSKamburova18@codingburgas.bg>
2022-10-24 12:04:00 +0300

    Initial commit

---
+++ src/main/java/com/example/opit123/Opit123Application.java
@@ -0,0 +1,13 @@
+package com.example.opit123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Opit123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Opit123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/opit123/Opit123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opit123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Opit123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
